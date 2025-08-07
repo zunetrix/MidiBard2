6470421126a50d1b1e1a636114164b34a92705a7
GiR_Zippo <gir_zippo@hellokitty.com>
2025-08-07 03:35:13 +0200

    Add XIVMIDIlib

---
+++ BardMusicPlayer.XIVMIDI/IO/RequestBuilder.cs
@@ -0,0 +1,55 @@
+﻿namespace BardMusicPlayer.XIVMIDI.IO;
+
+/// <summary>
+/// Build the API request string
+/// </summary>
+public class RequestBuilder
+{
+    private readonly string ApiBaseUrl = "https://api.xivmidi.com";
+
+    public string md5 { get; set; } = "";
+
+    /// <summary>
+    /// Set the editor
+    /// </summary>
+    public string Editor { get; set; } = "";
+
+    /// <summary>
+    /// Set the artist
+    /// </summary>
+    public string Artist { get; set; } = "";
+
+    /// <summary>
+    /// Set the title
+    /// </summary>
+    public string Title { get; set; } = "";
+
+    /// <summary>
+    /// Set the performer size
+    /// </summary>
+    public int bandSize { get; set; } = 0;
+
+    /// <summary>
+    /// Set the Tags
+    /// </summary>
+    public string Tags { get; set; } = "";
+
+    /// <summary>
+    /// Set the instruments "Piano;Harp"
+    /// </summary>
+    public string Instrument { get; set; } = "";
+    public int limit { get; set; } = -1;
+
+    public string BuildRequest()
+    {
+        var request = ApiBaseUrl + "/public/files?";
+        request += md5 == "" ? "" : "md5=" + md5 + "&";
+        request += Editor == "" ? "" : "editor=" + Editor + "&";
+        request += Artist == "" ? "" : "artist=" + Artist + "&";
+        request += Title == "" ? "" : "title=" + Title + "&";
+        request += bandSize <= 0 || bandSize > 8 ? "" : "bandSize=" + Misc.PerformerSize[bandSize] + "&";
+        request += Tags == "" ? "" : "tags=" + Tags + "&";
+        request += Instrument == "" ? "" : "instrument=" + Instrument;
+        return request;
+    }
+}
---
+++ BardMusicPlayer.XIVMIDI/IO/ResponseContainer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BardMusicPlayer.XIVMIDI.IO
+{
+    /// <summary>
+    /// The api responses
+    /// </summary>
+    public static class ResponseContainer
+    {
+        /// <summary>
+        /// The container for downloaded midi file
+        /// </summary>
+        public record MidiFile
+        {
+            /// <summary>
+            /// Filename of the downloaded midi
+            /// </summary>
+            public string Filename { get; set; } = "";
+
+            /// <summary>
+            /// Binary data reqdy to use via File.WriteAllBytes
+            /// </summary>
+            public byte[] data { get; set; } = null;
+        }
+
+        /// <summary>
+        /// The API response
+        /// </summary>
+        public record ApiResponse
+        {
+            public int status { get; set; }
+            public string message { get; set; }
+            public JsonData data {get; set;}
+        }
+
+        /// <summary>
+        /// The inner data
+        /// </summary>
+        public record JsonData
+        {
+            public List<File> files { get; set; }
+            public int totalFiles { get; set; }
+        }
+
+        /// <summary>
+        /// The file structure
+        /// </summary>
+        public record File
+        {
+            public string md5 { get; set; }
+            public string editorDiscordId { get; set; }
+            public string editor { get; set; }
+            public string artist { get; set; }
+            public string title { get; set; }
+            public string bandSize { get; set; }
+            public string source { get; set; }  
+            public List<string> tags { get; set; }
+            public int songDuration { get; set; }
+            public List<Track> tracks { get; set; }
+            public bool BMLPublished { get; set; }
+            public bool BMLEditorChannelPublished { get; set; }
+            public bool BMPPublished { get; set; }
+            public bool BardMetalPublished { get; set; }
+            public bool websitePublished { get; set; }
+            public bool publicAPIPublished { get; set; }
+            public string uploadedFrom { get; set; }
+            public DateTime createdAt { get; set; }
+            public DateTime updatedAt { get; set; }
+            public string BMLDiscordLink { get; set; }
+            public string BMLDiscordMessageId { get; set; }
+            public string BMLEditorChannelLink { get; set; }
+            public string BMLEditorChannelMessageId { get; set; }
+            public string BMPDiscordLink { get; set; }
+            public string BMPDiscordMessageId { get; set; }
+            public string BardMetalDiscordLink { get; set; }
+            public string BardMetalDiscordMessageId { get; set; }
+            public string websiteFilePath { get; set; }
+            public string websiteLink { get; set; }
+        }
+
+        /// <summary>
+        /// And the track structure
+        /// </summary>
+        public record Track
+        {
+            public int order { get; set; }
+            public string name { get; set; }
+            public string instrument { get; set; }
+            public int modifier { get; set; }
+        }
+    }
+}

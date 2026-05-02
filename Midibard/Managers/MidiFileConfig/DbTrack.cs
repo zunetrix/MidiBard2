5d74b34f7863719b93b45c7830d4fb80c9e7564e
Diego <allan_diego@hotmail.com>
2026-05-02 10:58:42 -0300

    json invalid non ulong cids

--- Midibard/Managers/MidiFileConfig/DbTrack.cs
+++ Midibard/Managers/MidiFileConfig/DbTrack.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MidiBard.Managers;
 
@@ -9,6 +13,8 @@ internal class DbTrack
     public string Name;
     public int Transpose;
     public uint Instrument;
+    // fix old json with not ulong values like -1
+    [JsonConverter(typeof(ULongListIgnoringNegativeConverter))]
     public List<ulong> AssignedCids = new List<ulong>();
 }
 
@@ -18,3 +24,30 @@ internal class DbTrack
 //     public int Instrument;
 //     public List<long> AssignedCids = new List<long>();
 // }
+
+public class ULongListIgnoringNegativeConverter : JsonConverter<List<ulong>>
+{
+    public override List<ulong> ReadJson(JsonReader reader, Type objectType, List<ulong> existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        var list = new List<ulong>();
+
+        var array = JArray.Load(reader);
+        foreach (var token in array)
+        {
+            if (token.Type == JTokenType.Integer)
+            {
+                long value = token.Value<long>();
+
+                if (value >= 0)
+                    list.Add((ulong)value);
+            }
+        }
+
+        return list;
+    }
+
+    public override void WriteJson(JsonWriter writer, List<ulong> value, JsonSerializer serializer)
+    {
+        serializer.Serialize(writer, value);
+    }
+}

2523f11a237f56ac7d8328549cb54c4773069623
Diego <allan_diego@hotmail.com>
2026-05-18 19:09:43 -0300

    add custom converter to handle invalid json cids (0 and negative)

--- Midibard/Managers/MidiFileConfig/DbTrack.cs
+++ Midibard/Managers/MidiFileConfig/DbTrack.cs
@@ -13,41 +13,54 @@ internal class DbTrack
     public string Name;
     public int Transpose;
     public uint Instrument;
-    // fix old json with not ulong values like -1
-    [JsonConverter(typeof(ULongListIgnoringNegativeConverter))]
+    [JsonConverter(typeof(SafeULongListConverter))]
     public List<ulong> AssignedCids = new List<ulong>();
 }
 
-// internal class DbChannel
-// {
-//     public int Transpose;
-//     public int Instrument;
-//     public List<long> AssignedCids = new List<long>();
-// }
-
-public class ULongListIgnoringNegativeConverter : JsonConverter<List<ulong>>
+// custom parser that handles invalid file values
+public class SafeULongListConverter : JsonConverter<List<ulong>>
 {
-    public override List<ulong> ReadJson(JsonReader reader, Type objectType, List<ulong> existingValue, bool hasExistingValue, JsonSerializer serializer)
+    public override List<ulong> ReadJson(
+        JsonReader reader, Type objectType,
+        List<ulong> existingValue, bool hasExistingValue,
+        JsonSerializer serializer)
     {
         var list = new List<ulong>();
-
         var array = JArray.Load(reader);
+
         foreach (var token in array)
         {
-            if (token.Type == JTokenType.Integer)
-            {
-                long value = token.Value<long>();
+            if (token.Type != JTokenType.Integer)
+                continue;
 
-                if (value >= 0)
-                    list.Add((ulong)value);
-            }
+            long cid = token.Value<long>();
+
+            // ignores 0 and negative cids
+            if (cid > 0)
+                list.Add((ulong)cid);
         }
 
         return list;
     }
 
-    public override void WriteJson(JsonWriter writer, List<ulong> value, JsonSerializer serializer)
+    public override void WriteJson(
+        JsonWriter writer, List<ulong> value,
+        JsonSerializer serializer)
     {
-        serializer.Serialize(writer, value);
+        writer.WriteStartArray();
+
+        if (value != null)
+        {
+            foreach (var cid in value)
+            {
+                // ignores 0 and negative cids
+                if (cid == 0)
+                    continue;
+
+                writer.WriteValue(cid);
+            }
+        }
+
+        writer.WriteEndArray();
     }
 }
--- Midibard/Managers/MidiFileConfig/DefaultPerformer.cs
+++ Midibard/Managers/MidiFileConfig/DefaultPerformer.cs
@@ -1,8 +1,68 @@
+using System;
 using System.Collections.Generic;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MidiBard.Managers;
 
 internal class DefaultPerformer
 {
+    [JsonConverter(typeof(SafeULongDictionaryConverter))]
     public Dictionary<ulong, List<int>> TrackMappingDict = new Dictionary<ulong, List<int>>();
+}
+
+// custom parser that handles invalid file values
+public class SafeULongDictionaryConverter : JsonConverter<Dictionary<ulong, List<int>>>
+{
+    public override Dictionary<ulong, List<int>> ReadJson(
+        JsonReader reader,
+        Type objectType,
+        Dictionary<ulong, List<int>>? existingValue,
+        bool hasExistingValue,
+        JsonSerializer serializer)
+    {
+        var result = new Dictionary<ulong, List<int>>();
+        var obj = JObject.Load(reader);
+
+        foreach (var prop in obj.Properties())
+        {
+            if (!ulong.TryParse(prop.Name, out var key) || key == 0)
+                continue;
+
+            try
+            {
+                var list = prop.Value.ToObject<List<int>>(serializer) ?? new List<int>();
+                result[key] = list;
+            }
+            catch
+            {
+                DalamudApi.PluginLog.Warning($"Invalid DefaultPerformer JSON value {key}: {result[key]}");
+            }
+        }
+
+        return result;
+    }
+
+    public override void WriteJson(
+    JsonWriter writer,
+    Dictionary<ulong, List<int>>? value,
+    JsonSerializer serializer)
+    {
+        writer.WriteStartObject();
+
+        if (value != null)
+        {
+            foreach (var kvp in value)
+            {
+                if (kvp.Key == 0)
+                    continue;
+
+                writer.WritePropertyName(kvp.Key.ToString());
+                serializer.Serialize(writer, kvp.Value);
+            }
+        }
+
+        writer.WriteEndObject();
+    }
 }

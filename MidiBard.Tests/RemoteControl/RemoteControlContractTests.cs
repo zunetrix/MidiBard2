60120fb621ab28e123bcf37d73abfb23a37ab52a
Rahul Vaidya <github@rvaidya.com>
2026-09-06 11:27:18 -0700

    Test shuffle remote mode compatibility

--- MidiBard.Tests/RemoteControl/RemoteControlContractTests.cs
+++ MidiBard.Tests/RemoteControl/RemoteControlContractTests.cs
@@ -100,6 +100,22 @@ public class RemoteControlContractTests
     }
 
     [Fact]
+    public void RandomEnumValueIsExposedAsShuffle()
+    {
+        RemoteControlService.ToWirePlayMode(PlayMode.Random)
+            .ShouldBe("shuffle");
+    }
+
+    [Theory]
+    [InlineData("shuffle")]
+    [InlineData("random")]
+    public void ShuffleParserAcceptsCurrentAndLegacyWireNames(string value)
+    {
+        RemoteControlService.ParsePlayMode(value)
+            .ShouldBe(PlayMode.Random);
+    }
+
+    [Fact]
     public void RichPlaylistPreservesFileNameWhileAddingLibraryMetadata()
     {
         var response = new PlaylistResponse(
